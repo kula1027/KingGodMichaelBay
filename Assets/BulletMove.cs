bab03eca302442800f844222d4e54f06e0f50ce6
wonjaechoi <wvvwvwwv@naver.com>
2016-11-21 06:53:13 +0900

    dd

---
+++ Assets/BulletMove.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMove : MonoBehaviour {
+    public Vector3 direction;
+    public float power;
+    public Rigidbody rb;
+    public bool impacted;
+    public Vector3 forceVec;
+    public float impactThreshold;
+    public bool firstImpact;
+
+
+	void FixedUpdate () {
+        if (!impacted)
+        {
+            rb.velocity = forceVec;
+
+            forceVec += Physics.gravity * Time.deltaTime;
+        }
+        /*else {
+            forceVec = Vector3.zero;
+            rb.velocity = Vector3.zero;
+            return;
+        }*/
+    }
+
+	public void Shoot(Vector3 direction_, float power_){
+		direction = direction_;
+		power = power_;
+		rb = gameObject.GetComponent<Rigidbody>();
+		forceVec = 100 * power * direction;
+		impacted = false;
+		firstImpact = false;
+	}
+
+    private float getEudDist(Vector3 v)
+    {
+        return Mathf.Sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+    }
+    void OnCollisionEnter(Collision col)
+    {
+		Debug.Log (col.gameObject);
+        if (col.relativeVelocity != Vector3.zero)
+        {
+            Vector3 v = col.relativeVelocity;
+            float damage = getEudDist(v);
+            if (damage > impactThreshold)
+            {
+                impacted = true;
+				Instantiate (Resources.Load ("Detonator", typeof(GameObject)), transform.position, Quaternion.identity);
+                //detonator.Explode();
+
+                Destroy(this.gameObject);
+                
+            }
+        }
+    }
+}
---
+++ Assets/PlayerTankController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerTankController : MonoBehaviour {
+
+	private Transform tankHead;
+	private Image powerUI;
+
+	void Start () {
+		tankHead = transform.FindChild ("Tower");
+		powerUI = GameObject.Find ("Canvas").transform.FindChild ("PowerBack").FindChild ("PowerFront").GetComponent<Image> ();
+	}
+		
+	private float moveSpeed = 10f;
+	private float rotateSpeed = 50f;
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (transform.right * moveSpeed * Time.deltaTime, Space.World);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (-transform.right * moveSpeed * Time.deltaTime / 2, Space.World);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate (-transform.up * rotateSpeed * Time.deltaTime, Space.World);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate (transform.up * rotateSpeed * Time.deltaTime, Space.World);
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+		
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			tankHead.transform.Rotate (-transform.up * rotateSpeed * Time.deltaTime, Space.World);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			tankHead.transform.Rotate (transform.up * rotateSpeed * Time.deltaTime, Space.World);
+		}
+
+		if (Input.GetKey (KeyCode.Space)) {
+			AddPower (Time.deltaTime);
+			if (power > 1f) {
+				SetPower (1f);
+			}
+		}
+		if(Input.GetKeyUp (KeyCode.Space)){
+			(Instantiate(Resources.Load("bullet",typeof(GameObject)), transform.position + new Vector3(0,3,0), Quaternion.identity) as GameObject).GetComponent<BulletMove>().Shoot(tankHead.transform.up + new Vector3(0f,0.3f,0f), power);
+			SetPower (0f);
+		}
+	}
+	private float power = 0f;
+
+	private void SetPower(float power_){
+		power = power_;
+		powerUI.fillAmount = power;
+	}
+	private void AddPower(float power_){
+		power += power_;
+		powerUI.fillAmount = power;
+	}
+}
